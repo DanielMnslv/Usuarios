5136b8fa77f8c0fa1185f701607e81954212141e
DanielMnslv <176536613+DanielMnslv@users.noreply.github.com>
2024-08-10 11:28:48 -0500

    Update urls.py

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,23 +1,14 @@
-"""
-URL configuration for usuarios project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
-from perfiles.views import SignUpView, BienvenidaView, SignInView
+from perfiles.views import (
+    SignUpView,
+    BienvenidaView,
+    SignInView,
+    SolicitudView,
+    OrdenView,
+    AnticipoView,
+    DiarioView,
+)
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -26,8 +17,8 @@ urlpatterns = [
     path("", BienvenidaView.as_view(), name="bienvenida"),
     path("registrate/", SignUpView.as_view(), name="sign_up"),
     path("login/", SignInView.as_view(), name="login"),
-    path("solicitud/", SignInView.as_view(), name="solicitud"),
-    path("orden/", SignInView.as_view(), name="orden"),
-    path("anticipo/", SignInView.as_view(), name="anticipo"),
-    path("diario/", SignInView.as_view(), name="diario"),
+    path("solicitud/", SolicitudView.as_view(), name="solicitud"),
+    path("orden/", OrdenView.as_view(), name="orden"),
+    path("anticipo/", AnticipoView.as_view(), name="anticipo"),
+    path("diario/", DiarioView.as_view(), name="diario"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
