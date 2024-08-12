a3e0437d60784659319ebe1e9e1a188f34cc466a
DanielMnslv <176536613+DanielMnslv@users.noreply.github.com>
2024-08-12 17:10:10 -0500

    Update models.py

--- perfiles/models.py
+++ perfiles/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django import forms
+from decimal import Decimal, ROUND_HALF_UP
 
 
 class Perfil(models.Model):
@@ -37,6 +38,7 @@ class Solicitud(models.Model):
     tipo = models.CharField(max_length=50)
     observaciones = models.TextField(blank=True, null=True)
     solicitado = models.CharField(max_length=255)
+    imagen = models.ImageField(upload_to="imagenes/", blank=True, null=True)
 
     def __str__(self):
         return self.nombre
@@ -51,6 +53,9 @@ class Orden(models.Model):
     destino = models.CharField(max_length=255)
     tiempo_entrega = models.CharField(max_length=255)
     observaciones = models.TextField(blank=True, null=True)
+    documento_pdf = models.FileField(
+        upload_to="documentos_pdf/", blank=True, null=True
+    )  # Campo para PDF
 
     def __str__(self):
         return f"{self.codigo_cotizacion} - {self.descripcion}"
@@ -64,21 +69,33 @@ class Anticipo(models.Model):
     producto_servicio = models.CharField(max_length=100)
     vlr_unitario = models.DecimalField(max_digits=10, decimal_places=2)
     subtotal = models.DecimalField(max_digits=10, decimal_places=2)
-    iva = models.DecimalField(max_digits=10, decimal_places=2)
-    retencion = models.DecimalField(max_digits=10, decimal_places=2)
-    total_pagar = models.DecimalField(max_digits=10, decimal_places=2)
+    iva = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    retencion = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
+    total_pagar = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
     observaciones = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return self.nombre
-
     def save(self, *args, **kwargs):
-        # Cálculos basados en los valores actuales del formulario
-        self.subtotal = self.cantidad * self.vlr_unitario
-        self.iva = self.subtotal * self.iva  # Aquí puede haber un problema
-        self.retencion = self.subtotal * self.retencion  # Aquí también
-        self.total_pagar = self.subtotal + self.iva - self.retencion
-        super(Anticipo, self).save(*args, **kwargs)
+        self.vlr_unitario = Decimal(self.vlr_unitario).quantize(
+            Decimal(".01"), rounding=ROUND_HALF_UP
+        )
+        self.subtotal = Decimal(self.cantidad * self.vlr_unitario).quantize(
+            Decimal(".01"), rounding=ROUND_HALF_UP
+        )
+        self.iva = Decimal(self.subtotal * (self.iva or 0)).quantize(
+            Decimal(".01"), rounding=ROUND_HALF_UP
+        )
+        self.retencion = Decimal(self.subtotal * (self.retencion or 0)).quantize(
+            Decimal(".01"), rounding=ROUND_HALF_UP
+        )
+        self.total_pagar = Decimal(self.subtotal + self.iva - self.retencion).quantize(
+            Decimal(".01"), rounding=ROUND_HALF_UP
+        )
+
+        super().save(*args, **kwargs)
 
 
 class AnticipoForm(forms.ModelForm):
@@ -97,3 +114,61 @@ class AnticipoForm(forms.ModelForm):
             "total_pagar",
             "observaciones",
         ]
+
+
+class Diario(models.Model):
+    TIEMPO_ENTREGA_CHOICES = [(f"{i:02d}:00", f"{i:02d}:00") for i in range(24)]
+
+    tiempo_entrega = models.DateTimeField("Día y Hora de Entrega")
+    nombre = models.CharField(max_length=200)
+    empresa = models.CharField(max_length=200)
+    centro_costo = models.CharField(
+        max_length=200,
+        choices=[
+            ("ADMINISTRACIÓN", "ADMINISTRACIÓN"),
+            ("PRODUCCION", "PRODUCCION"),
+            ("ALEVINERA", "ALEVINERA"),
+            ("ECOPEZ", "ECOPEZ"),
+            ("FERRY", "FERRY"),
+            ("CARRO VNS228", "CARRO VNS228"),
+            ("CARRO WGY", "CARRO WGY"),
+            ("CARRO THS 473", "CARRO THS 473"),
+            ("CARRO PESCA SRP 254", "CARRO PESCA SRP 254"),
+            ("TERMOKIN GQZ 727", "TERMOKIN GQZ 727"),
+            ("TERMOKIN GRK 030", "TERMOKIN GRK 030"),
+            ("THERMO KING THS 592", "THERMO KING THS 592"),
+            ("UNIDAD COMERCIALIZACION", "UNIDAD COMERCIALIZACION"),
+        ],
+    )
+    destino = models.CharField(
+        max_length=200,
+        choices=[
+            ("ADMINISTRACIÓN", "ADMINISTRACIÓN"),
+            ("PRODUCCION", "PRODUCCION"),
+            ("ALEVINERA", "ALEVINERA"),
+            ("ECOPEZ", "ECOPEZ"),
+            ("FERRY", "FERRY"),
+            ("CARRO VNS228", "CARRO VNS228"),
+            ("CARRO WGY", "CARRO WGY"),
+            ("CARRO THS 473", "CARRO THS 473"),
+            ("CARRO PESCA SRP 254", "CARRO PESCA SRP 254"),
+            ("TERMOKIN GQZ 727", "TERMOKIN GQZ 727"),
+            ("TERMOKIN GRK 030", "TERMOKIN GRK 030"),
+            ("THERMO KING THS 592", "THERMO KING THS 592"),
+            ("UNIDAD COMERCIALIZACION", "UNIDAD COMERCIALIZACION"),
+        ],
+    )
+    medio_pago = models.CharField(
+        max_length=200,
+        choices=[
+            ("efectivo", "Efectivo"),
+            ("tarjeta", "Tarjeta"),
+            ("transferencia", "Transferencia"),
+        ],
+    )
+    documento_pdf = models.FileField(
+        upload_to="documentos_pdf/", blank=True, null=True
+    )  # Campo para PDF
+
+    def __str__(self):
+        return f"Diario {self.nombre} - {self.empresa}"
