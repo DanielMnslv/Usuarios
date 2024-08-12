93715376a629dd9809becff151e9d31e733a9aeb
DanielMnslv <176536613+DanielMnslv@users.noreply.github.com>
2024-08-12 17:08:56 -0500

    Update urls.py

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,8 +6,12 @@ from perfiles.views import (
     SignInView,
     SolicitudView,
     OrdenView,
-    AnticipoView,
-    DiarioView,
+    anticipo_view,
+    diario_view,
+    ver_solicitudes,
+    ver_ordenes,
+    ver_diario,
+    custom_logout,
 )
 from django.conf.urls.static import static
 from django.conf import settings
@@ -18,7 +22,11 @@ urlpatterns = [
     path("registrate/", SignUpView.as_view(), name="sign_up"),
     path("login/", SignInView.as_view(), name="login"),
     path("solicitud/", SolicitudView.as_view(), name="solicitud"),
+    path("ver_solicitudes/", ver_solicitudes, name="ver_solicitudes"),
+    path("ver_ordenes/", ver_ordenes, name="ver_ordenes"),
+    path("ver_diario/", ver_diario, name="ver_diario"),
     path("orden/", OrdenView.as_view(), name="orden"),
-    path("anticipo/", AnticipoView.as_view(), name="anticipo"),
-    path("diario/", DiarioView.as_view(), name="diario"),
+    path("anticipo/", anticipo_view, name="anticipo"),
+    path("diario/", diario_view, name="diario"),
+    path("logout/", custom_logout, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
